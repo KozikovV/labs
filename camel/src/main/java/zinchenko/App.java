0dc59ffe22ff0499c4c287185c2a28fcd48d7e42
zinjvi <zinjvi@gmail.com>
2014-01-25 20:51:08 +0200

    changes in the camel module

--- camel/src/main/java/zinchenko/App.java
+++ camel/src/main/java/zinchenko/App.java
@@ -6,8 +6,8 @@ package zinchenko;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws Exception {
+        CamelFileCopier camelFileCopier = new CamelFileCopier();
+        camelFileCopier.copy();
     }
 }
