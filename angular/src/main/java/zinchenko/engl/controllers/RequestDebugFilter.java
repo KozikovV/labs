8d19f395adc9f246ec957e369d9d70b9be1d06d4
vitaliy-zinchenko <vitaliy.zinchenko90@gmail.com>
2014-05-09 21:58:07 +0300

    C

--- angular/src/main/java/zinchenko/engl/controllers/RequestDebugFilter.java
+++ angular/src/main/java/zinchenko/engl/controllers/RequestDebugFilter.java
@@ -4,6 +4,7 @@ import javax.servlet.*;
 import java.io.IOException;
 
 public class RequestDebugFilter implements Filter {
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -11,11 +12,13 @@ public class RequestDebugFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("Debug filter");
         try {
-            Thread.sleep(4000L);
+            Thread.sleep(500L);
         } catch (InterruptedException e) {
             new RuntimeException("Fail debug filter.", e);
         }
+        chain.doFilter(request, response);
     }
 
     @Override
